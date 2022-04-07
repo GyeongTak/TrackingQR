252917c8a1ac9ab5f375a45c1d19706f56d48c30
kmingki <wl1524@naver.com>
2022-04-07 20:31:18 +0900

    designer page filter button 추가

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,10 @@
 
+import DesignerPage from './pages/DesignerPage';
 
 function App() {
   return (
     <div style={{width:'100%'}}>
+      <DesignerPage />
     </div>
   );
 }
--- frontend/src/pages/DesignerPage.js
+++ frontend/src/pages/DesignerPage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import MainMenu from '../components/MainMenu';
-import { Card, Avatar } from 'antd';
+import { Card, Avatar, Dropdown, Button, Menu } from 'antd';
+import { HeartOutlined, HeartTwoTone, DownOutlined } from '@ant-design/icons';
 import 'antd/dist/antd.css';
 
 const dummyData = [
@@ -41,23 +42,54 @@ const dummyData = [
     },
 ];
 
+const menu = (
+    <Menu>
+      <Menu.Item>
+          식당/카페
+      </Menu.Item>
+      <Menu.Item>
+          식품/건강
+      </Menu.Item>
+      <Menu.Item>
+          병원
+      </Menu.Item>
+    </Menu>
+  );
+
 
 const DesignerPage = () => {
 
     return (
         <>
         <MainMenu />
-        <div className={'main-container'} style={{padding: '0 10%'}}>
-            <div className={'mainTitle'} style={{fontSize: '32px', fontWeight: 'bold', margin:'30px 0'}}>
+        <div className={'main-container'} style={{padding: '0 10%', width: '100%', height: '100%'}}>
+            <div className={'mainTitle'} style={{fontSize: '32px', fontWeight: 'bold', padding:'50px 0'}}>
                 디자이너를 찾고 계신가요?
             </div>
-            <div className='portfolio-container' style={{display: 'inline-grid', gridTemplateColumns: 'repeat(4, 1fr)', gap: '10px'}}>
+
+            <div style={{margin: '40px 0', width:'100%', position: 'relative'}}>
+                <div>
+                <Dropdown overlay={menu} placement="bottomLeft">
+                    <Button icon={<DownOutlined />} style={{marginRight: '10px'}}>업종</Button>
+                </Dropdown>
+                <Dropdown overlay={menu} placement="bottomLeft">
+                    <Button icon={<DownOutlined />} >스타일</Button>
+                </Dropdown>
+                </div>
+                <div style={{position: 'absolute', right:'0', top: '0'}}>
+                <Dropdown overlay={menu} placement="bottomLeft">
+                    <Button icon={<DownOutlined />} >최신순</Button>
+                </Dropdown>
+                </div>
+            </div>
+
+            <div className='portfolio-container' style={{width: '100%', display: 'inline-grid', gridTemplateColumns: 'repeat(auto-fill, minmax(20%, auto))', gap: '5%'}}>
             {dummyData.map((post)=>
                 <Card
                 hoverable
                 cover={<img alt="example" src={post.imageSrc} />}
               >
-                <Card.Meta avatar={<Avatar src={post.userProfilePhoto} />} title={post.userNickname} description={post.userEmail} />
+                <Card.Meta avatar={<Avatar src={post.userProfilePhoto} />} title={<div style={{position: 'relative'}}>{post.userNickname}<HeartTwoTone style={{position: 'absolute', right:'0px'}} twoToneColor='#ff69b4'/></div>} description={post.userEmail} />
               </Card>)}
             </div>
         </div>
