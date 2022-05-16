8b30031b54308e1bd481dea70ab114694c5a7eb0
kmingki <wl1524@naver.com>
2022-05-16 20:59:39 +0900

    인증페이지 RequireAuth 제작

---
+++ frontend/src/router/RequireAuth.js
@@ -0,0 +1,18 @@
+import { useRecoilState } from 'recoil';
+import userState from '../../src/store/user';
+import { Navigate, useLocation } from 'react-router-dom';
+
+export function RequireAuth({ children, ...rest }) {
+  const [user, setUser]  = useRecoilState(userState);
+    let location = useLocation();
+  
+    if (!user.userId) {
+      // Redirect them to the /login page, but save the current location they were
+      // trying to go to when they were redirected. This allows us to send them
+      // along to that page after they login, which is a nicer user experience
+      // than dropping them off on the home page.
+      return <Navigate to="/login" state={{ from: location }} replace />;
+    }
+  
+    return children;
+  }
