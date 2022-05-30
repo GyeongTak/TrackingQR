d1c9ba9c543d76dbab6593383fc55f05efdd6121
kmingki <wl1524@naver.com>
2022-05-30 21:20:06 +0900

    portfolio 등록 페이지 유저정보 수정

--- frontend/src/pages/CreatePortfolio/style.js
+++ frontend/src/pages/CreatePortfolio/style.js
@@ -1,15 +1,18 @@
 import styled from '@emotion/styled';
 
 const UserInfoForm = styled.div`
-    padding:50px 0;
+    padding:50px 20px;
     display: flex;
     flex-wrap: wrap;
     align-items: center;
+    border-radius: 1%;
+    background-color: #f0f0f1;
+    
 `
 
 const NameWrapper = styled.div`
-    font-size :22px;
-    font-weight: 500;
+    font-size : 18px;
+    font-weight: 400;
     margin-left: 20px;
     
 `
